20c4560c9f6027ff46b38709d049935a87ae2aff
Harry <harryrock67@gmail.com>
2018-11-20 01:51:25 -0500

    Example from code pen

--- dev/dcss.js
+++ dev/dcss.js
@@ -1,7 +1,7 @@
 //author = @harryPunia
 
 var DCSS = function (domElement) {
-    domElement == undefined ? this.domElement = document : domElement == document ? this.domElement = document : this.domElement = document.getElementById(domElement);
+    domElement == undefined ? (this.domElement = document, console.warn('Missing target, DCss will use the entire document as a fallback. Pass document/id as a parameter.')) : domElement == document ? this.domElement = document : this.domElement = document.getElementById(domElement);
     this.offsetX = 50;
     this.offsetY = 40;
     this.snapViewer = false;
@@ -17,6 +17,7 @@ var DCSS = function (domElement) {
 
     this.init = function () {
         this.view = document.createElement('div');
+        this.view.id = 'viewer';
         document.body.appendChild(this.view);
         this.css.view = this.view.style;
         //---------------------------------
@@ -52,21 +53,24 @@ var DCSS = function (domElement) {
             } else {
                 console.olog = () => {};
             }
-            console.log = message => {
-                console.olog(message);
-                if (prevMessage == message) {} else {
-                    this.dConsole = document.createElement('pre');
-                    let dConsoleLog = document.createTextNode(message);
-                    this.dConsole.classList.add('__viewLog__');
-                    this.dConsole.append(dConsoleLog);
-                    this.view.append(this.dConsole);
-                    this.view.scrollTop = this.view.scrollHeight - this.view.clientHeight;
-                }
-                prevMessage = message;
-            };
-            console.error = console.debug = console.info = console.log;
         }
+
+        console.log = message => {
+            console.olog(message);
+            if (prevMessage == message) {} else {
+                message.toString();
+                this.dConsole = document.createElement('pre');
+                let dConsoleLog = document.createTextNode(message);
+                this.dConsole.classList.add('__viewLog__');
+                this.dConsole.append(dConsoleLog);
+                this.view.append(this.dConsole);
+                this.view.scrollTop = this.view.scrollHeight - this.view.clientHeight;
+                prevMessage = message;
+            }
+        }
+        console.warn = console.error = console.info = console.info = console.log;
     }
+
     this.snapView = e => {
         if (this.snapping && this.snapViewer) {
             this.css.view.left = e.clientX - (this.width / 2) + 'px';
@@ -76,7 +80,154 @@ var DCSS = function (domElement) {
 
     //init
     this.init();
-    console.log('hey');
+
+    (function initConsoleLogDiv() {
+        'use strict';
+
+
+        if (console.log.toDiv) {
+            return;
+        }
+
+        function toString(x) {
+            return typeof x === 'string' ? x : JSON.stringify(x);
+        }
+
+        var log = console.log.bind(console);
+        var error = console.error.bind(console);
+        var warn = console.warn.bind(console);
+        var table = console.table ? console.table.bind(console) : null;
+        var consoleId = 'console-log-div';
+
+        //        // Create the Console Div container.
+        //        function createOuterElement(id) {
+        //            var outer = document.getElementById(id);
+        //            if (!outer) {
+        //                outer = document.createElement('div');
+        //                outer.id = id;
+        //                document.body.appendChild(outer);
+        //            }
+        //            var style = outer.style;
+        //            return outer;
+        //        }
+        // Create the logging div and adornments.
+        var logTo = (function createLogDiv() {
+            var legend = document.createElement('div');
+            legend.id = "legend";
+            document.getElementById('viewer').appendChild(legend);
+            var div = document.createElement('div');
+            div.id = 'console-log-text';
+            document.getElementById('viewer').appendChild(div);
+            return div;
+        }());
+
+        function printToDiv() {
+            var msg = Array.prototype.slice.call(arguments, 0)
+                .map(toString)
+                .join(' ');
+            var item = document.createElement('div');
+            item.classList.add('log-row');
+            item.textContent = msg;
+            logTo.appendChild(item);
+        }
+
+        function logWithCopy() {
+            var ele = document.getElementById('console-log-div');
+            setDarkLight(ele);
+            log.apply(null, arguments);
+            printToDiv.apply(null, arguments);
+        }
+
+        console.log = logWithCopy;
+        console.log.toDiv = true;
+
+        console.error = function errorWithCopy() {
+            error.apply(null, arguments);
+            var args = Array.prototype.slice.call(arguments, 0);
+            args.unshift('ERROR:');
+            printToDiv.apply(null, args);
+        };
+
+        console.warn = function logWarning() {
+            warn.apply(null, arguments);
+            var args = Array.prototype.slice.call(arguments, 0);
+            args.unshift('WARNING:');
+            printToDiv.apply(null, args);
+        };
+
+        function printTable(objArr, keys) {
+
+            var numCols = keys.length;
+            var len = objArr.length;
+            var $table = document.createElement('table');
+            $table.style.width = '100%';
+            $table.setAttribute('border', '1');
+            var $head = document.createElement('thead');
+            var $tdata = document.createElement('td');
+            $tdata.innerHTML = 'Index';
+            $head.appendChild($tdata);
+
+            for (var k = 0; k < numCols; k++) {
+                $tdata = document.createElement('td');
+                $tdata.innerHTML = keys[k];
+                $head.appendChild($tdata);
+            }
+            $table.appendChild($head);
+
+            for (var i = 0; i < len; i++) {
+                var $line = document.createElement('tr');
+                $tdata = document.createElement('td');
+                $tdata.innerHTML = i;
+                $line.appendChild($tdata);
+
+                for (var j = 0; j < numCols; j++) {
+                    $tdata = document.createElement('td');
+                    $tdata.innerHTML = objArr[i][keys[j]];
+                    $line.appendChild($tdata);
+                }
+                $table.appendChild($line);
+            }
+            var div = document.getElementById('console-log-text');
+            div.appendChild($table);
+
+        }
+
+        console.table = function logTable() {
+            if (typeof table === 'function') {
+                table.apply(null, arguments);
+            }
+
+            var objArr = arguments[0];
+            var keys;
+
+            if (typeof objArr[0] !== 'undefined') {
+                keys = Object.keys(objArr[0]);
+            }
+            printTable(objArr, keys);
+        };
+
+        window.addEventListener('error', function (err) {
+            printToDiv('EXCEPTION:', err.message + '\n  ' + err.filename, err.lineno + ':' + err.colno);
+        });
+
+        //Detect dark or light colors.
+        function setDarkLight(element) {
+            var color = window.getComputedStyle(element, null).backgroundColor;
+            if (isDark(color)) {
+                element.style.color = "rgba(255,255,255,1)";
+            } else {
+                element.style.color = "rgba(0,0,0,.61)";
+            }
+        }
+
+        function isDark(color) {
+            var match = /rgb\((\d+).*?(\d+).*?(\d+)\)/.exec(color);
+            return parseFloat(match[1]) +
+                parseFloat(match[2]) +
+                parseFloat(match[3]) <
+                3 * 256 / 2; // r+g+b should be less than half of max (3 * 256)
+        }
+    }());
     //Additional
     this.domElement.addEventListener('mouseout', () => {
         !this.snapViewer ? this.css.view.display = 'none' : 0
