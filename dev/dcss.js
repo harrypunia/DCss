865562ff3bf74d227b90862a9a8e4562eff8ae89
Harry <harryrock67@gmail.com>
2018-11-19 20:05:02 -0500

    Remove Animations
    //Viewer follows on scroll
    //Issue: on scroll viewer disappearing

--- dev/dcss.js
+++ dev/dcss.js
@@ -9,11 +9,11 @@ var DCSS = function (domElement) {
         css;
 
     //Methods
-    this.display = function () {
+    this.display = () => {
         this.information();
         this.html();
     }
-    this.information = function () {
+    this.information = () => {
 
     }
     this.html = function () {
@@ -24,45 +24,38 @@ var DCSS = function (domElement) {
         css = view.style;
         this.initCss();
     }
-    this.initCss = function () {
-        css.width = '20px';
-        css.height = '10px';
+    this.initCss = () => {
         css.top = '0px';
         css.left = '0px';
-        css.cssText = 'width: 20px; height: 10px; transition: width 0.15s, height 0.15s; background: rgb(239, 239, 239); border-radius: 5px; box-shadow: rgba(0, 0, 0, 0.4) 0px 0px 50px; padding: 20px; position: absolute; z-index: 99999999;';
+        css.cssText = 'width: 500px; height: 700px; background: rgb(239, 239, 239); border-radius: 5px; box-shadow: rgba(0, 0, 0, 0.4) 0px 0px 50px; padding: 20px; position: absolute; z-index: 99999999;';
     }
-    this.updateCss = function (e) {
+    this.updateCss = e => {
         let x = e.clientX,
             y = e.clientY,
             switchX = x > (window.innerWidth / 2),
-            switchY = y > (window.innerHeight / 2);
-
-        if (switchX == true) {
-            css.left = (x - scope.offsetX - parseInt(css.width)) + 'px';
-        } else if (switchX == false) {
-            css.left = x + scope.offsetX + 'px';
+            switchY = y > (window.innerHeight / 2),
+            scrollingSnap = {
+                sX: x,
+                sy: y
+            };
+        if (switchX) {
+            css.left = (x - scope.offsetX + window.pageXOffset - parseInt(css.width)) + 'px';
+        } else if (!switchX) {
+            css.left = x + scope.offsetX + window.pageXOffset + 'px';
         }
-        if (switchY == true) {
-            css.top = (y - scope.offsetY - parseInt(css.height)) + 'px';
-        } else if (switchY == false) {
-            css.top = y + scope.offsetY + 'px';
+        if (switchY) {
+            css.top = (y - scope.offsetY + window.pageYOffset - parseInt(css.height)) + 'px';
+        } else if (!switchY) {
+            css.top = y + scope.offsetY + window.pageYOffset + 'px';
         }
-        console.log(css.top + scope.offsetY);
     }
-    this.hideView = function () {
-        css.width = '20px';
-        css.height = '10px';
+    this.hideView = () => {
         css.top = '-100vh';
         css.left = '-100vw';
-        css.transition = 'width .3s, height .3s';
     }
-    this.showView = function () {
-        css.width = '500px';
-        css.height = '700px';
-        css.transition = 'width .3s, height .3s';
-    }
+    this.showView = () => {}
 
-    this.holdView = function () {
+    this.holdView = () => {
         let snapX = css.left,
             snapY = css.top;
         css.left = snapX + 'px !important';
@@ -70,7 +63,8 @@ var DCSS = function (domElement) {
     }
 
     //user Function
-    this.view = function (userView) {
+    this.view = userView => {
+        //Work on this
         view = document.getElementById(userView);
         css = view.style;
     }
@@ -79,8 +73,9 @@ var DCSS = function (domElement) {
     this.domElement.addEventListener('mouseout', scope.hideView, false);
     this.domElement.addEventListener('mouseover', scope.showView, false);
     window.onkeydown = e => {
-        let key = e.code ? e.which : e.which;
+        let key = e.keyCode ? e.which : e.which;
         console.log(key);
     };
+    this.domElement.addEventListener('scroll', scope.updateCss, false);
     this.domElement.addEventListener('mousemove', scope.updateCss, false);
 }
